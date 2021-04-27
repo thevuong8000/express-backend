fbfd5f0f5cd74aaf227f584daaffede8e6a65a3a
dante <ducmanh.tran2904@gmail.com>
2021-04-28 06:49:44 +0700

    refactor: Remove deprecated body-parser

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 
+const db = require('./queries');
+
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('I Love you');
+  res.json({ type: 'success', message: 'Hello World!' });
 });
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
