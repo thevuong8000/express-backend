87f38ad8cdf74462d99f4c30bca65d0c67337502
dante <ducmanh.tran2904@gmail.com>
2021-05-16 02:37:11 +0700

    feat: Pseudo set up swagger docs

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
+// const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const apiDocs = require('./src/swagger/api.json');
 
 const app = express();
 const port = 8000;
 
+const SWAGGER_OPTIONS = {
+	explorer: true
+};
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(apiDocs, SWAGGER_OPTIONS));
+
 app.get('/', (req, res) => {
-  res.send('I Love you');
+	res.send('I Love you');
+});
+
+app.get('/foo', (req, res) => {
+	res.send({ foo: 1, bar: 'bar' });
+});
+
+app.get('/bar', (req, res) => {
+	res.send({ foo: 'foo', bar: 1 });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
