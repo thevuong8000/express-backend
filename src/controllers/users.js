c3374adbe3419e5d5b37194eb100e687ba5dad4f
dante <ducmanh.tran2904@gmail.com>
2021-05-16 20:59:26 +0700

    refactor: Move router callback to controllers

---
+++ src/controllers/users.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+
+exports.getUsers = (req, res, next) => {
+	User.find()
+		.then((users) => res.status(200).json({ users }))
+		.catch((error) => res.status(400).json({ error }));
+};
+
+exports.createUser = (req, res, next) => {
+	const { username, password } = req.body;
+	const user = new User({ username, password });
+	user
+		.save()
+		.then((result) => res.status(201).json({ user: result, message: 'ok' }))
+		.catch((error) => res.status(400).json({ error }));
+};
+
+exports.getUsersById = (req, res, next) => {
+	const { id } = req.params;
+	User.findOne({ _id: id })
+		.then((user) => res.status(200).json({ user }))
+		.catch((error) => res.status(400).json({ error }));
+};
+
+exports.updateUser = (req, res, next) => {
+	const { id } = req.params;
+	const { username, password } = req.body;
+
+	const newUser = new User({ _id: id, username, password });
+	User.updateOne({ _id: id }, newUser)
+		.then(() => res.status(200).json({ message: 'Successfully modified' }))
+		.catch((error) => res.status(400).json({ error }));
+};
+
+exports.deleteUser = (req, res, next) => {
+	const { id } = req.params;
+
+	User.deleteOne({ _id: id })
+		.then((result) => res.status(200).json({ message: 'Successfully deleted' }))
+		.catch((error) => res.status(400).json({ error }));
+};
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,46 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {
+	createUser,
+	getUsers,
+	getUsersById,
+	updateUser,
+	deleteUser
+} = require('../../controllers/users');
 
-const User = require('../../models/user');
-
-router.get('/', (req, res, next) => {
-	User.find()
-		.then((users) => res.status(200).json({ users }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-router.post('/', (req, res, next) => {
-	const { username, password } = req.body;
-	const user = new User({ username, password });
-	user
-		.save()
-		.then((result) => res.status(201).json({ user: result, message: 'ok' }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-router.get('/:id', (req, res, next) => {
-	const { id } = req.params;
-	User.findOne({ _id: id })
-		.then((user) => res.status(200).json({ user }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-router.put('/:id', (req, res, next) => {
-	const { id } = req.params;
-	const { username, password } = req.body;
-
-	const newUser = new User({ _id: id, username, password });
-	User.updateOne({ _id: id }, newUser)
-		.then(() => res.status(200).json({ message: 'Successfully modified' }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-router.delete('/:id', (req, res, next) => {
-	const { id } = req.params;
-
-	User.deleteOne({ _id: id })
-		.then((result) => res.status(200).json({ message: 'Successfully deleted' }))
-		.catch((error) => res.status(400).json({ error }));
-});
+router.get('/', getUsers);
+router.post('/', createUser);
+router.get('/:id', getUsersById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
