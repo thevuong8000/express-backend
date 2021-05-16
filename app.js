b97e85cd97bf281994a93d31dca6ffb17a1b4704
dante <ducmanh.tran2904@gmail.com>
2021-05-16 17:13:16 +0700

    feat: Add CURD for users

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const swaggerUI = require('swagger-ui-express');
 const apiDocs = require('./src/swagger/api.json');
 
 const app = express();
+
+const User = require('./src/models/user');
+
 const mongoUser = 'dante';
 const mongoPassword = 'fWd5BIGTlsmCwC2Q';
-const MONGO_URI = `mongodb+srv://${mongoUser}:${mongoPassword}@test-mongo.e76gm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const databaseName = 'userManagement';
+const MONGO_URI = `mongodb+srv://${mongoUser}:${mongoPassword}@test-mongo.e76gm.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
 
 mongoose
 	.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -36,6 +40,45 @@ app.use((req, res, next) => {
 	next();
 });
 
-// mongoDB password: fWd5BIGTlsmCwC2Q
+// Rest-API
+app.get('/users', (req, res, next) => {
+	User.find()
+		.then((users) => res.status(200).json({ users }))
+		.catch((error) => res.status(400).json({ error }));
+});
+
+app.post('/users', (req, res, next) => {
+	const { username, password } = req.body;
+	const user = new User({ username, password });
+	user
+		.save()
+		.then((result) => res.status(201).json({ user: result, message: 'ok' }))
+		.catch((error) => res.status(400).json({ error }));
+});
+
+app.get('/users/:id', (req, res, next) => {
+	const { id } = req.params;
+	User.findOne({ _id: id })
+		.then((user) => res.status(200).json({ user }))
+		.catch((error) => res.status(400).json({ error }));
+});
+
+app.put('/users/:id', (req, res, next) => {
+	const { id } = req.params;
+	const { username, password } = req.body;
+
+	const newUser = new User({ _id: id, username, password });
+	User.updateOne({ _id: id }, newUser)
+		.then(() => res.status(200).json({ message: 'Successfully modified' }))
+		.catch((error) => res.status(400).json({ error }));
+});
+
+app.delete('/users/:id', (req, res, next) => {
+	const { id } = req.params;
+
+	User.deleteOne({ _id: id })
+		.then((result) => res.status(200).json({ message: 'Successfully deleted' }))
+		.catch((error) => res.status(400).json({ error }));
+});
 
 module.exports = app;
