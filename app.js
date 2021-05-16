62b3c84a6f259a8afea9c526cd7eafa818005097
dante <ducmanh.tran2904@gmail.com>
2021-05-16 20:21:45 +0700

    refactor: Split router instead of using express app for all

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const swaggerUI = require('swagger-ui-express');
 const apiDocs = require('./src/swagger/api.json');
 
 const app = express();
-
-const User = require('./src/models/user');
+const userRoutes = require('./src/routes/api/users');
 
 const mongoUser = 'dante';
 const mongoPassword = 'fWd5BIGTlsmCwC2Q';
@@ -41,44 +40,6 @@ app.use((req, res, next) => {
 });
 
 // Rest-API
-app.get('/users', (req, res, next) => {
-	User.find()
-		.then((users) => res.status(200).json({ users }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-app.post('/users', (req, res, next) => {
-	const { username, password } = req.body;
-	const user = new User({ username, password });
-	user
-		.save()
-		.then((result) => res.status(201).json({ user: result, message: 'ok' }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-app.get('/users/:id', (req, res, next) => {
-	const { id } = req.params;
-	User.findOne({ _id: id })
-		.then((user) => res.status(200).json({ user }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-app.put('/users/:id', (req, res, next) => {
-	const { id } = req.params;
-	const { username, password } = req.body;
-
-	const newUser = new User({ _id: id, username, password });
-	User.updateOne({ _id: id }, newUser)
-		.then(() => res.status(200).json({ message: 'Successfully modified' }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-app.delete('/users/:id', (req, res, next) => {
-	const { id } = req.params;
-
-	User.deleteOne({ _id: id })
-		.then((result) => res.status(200).json({ message: 'Successfully deleted' }))
-		.catch((error) => res.status(400).json({ error }));
-});
+app.use('/users', userRoutes);
 
 module.exports = app;
