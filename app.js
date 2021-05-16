733a2137159c0360a98e4c7d0a551aa08f32f85a
dante <ducmanh.tran2904@gmail.com>
2021-05-16 14:14:15 +0700

    feat: Connect mongoDB with moogoose

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const swaggerUI = require('swagger-ui-express');
 const apiDocs = require('./src/swagger/api.json');
 
 const app = express();
+const mongoUser = 'dante';
+const mongoPassword = 'fWd5BIGTlsmCwC2Q';
+const MONGO_URI = `mongodb+srv://${mongoUser}:${mongoPassword}@test-mongo.e76gm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose
+	.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('mongo connected');
+	})
+	.catch((err) => console.log('err'));
 
 // swagger api docs
 const SWAGGER_OPTIONS = {
@@ -24,5 +35,7 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 	next();
 });
+
+// mongoDB password: fWd5BIGTlsmCwC2Q
 
 module.exports = app;
