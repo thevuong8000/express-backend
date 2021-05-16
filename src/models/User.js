b979f64d6f06b48fb5e0eb22e9049e1ab3001c94
dante <ducmanh.tran2904@gmail.com>
2021-05-16 23:16:23 +0700

    feat: Add mongoose-unique-validator

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
 	username: { type: String, required: true },
 	password: { type: String, required: true }
 });
 
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('User', userSchema);
