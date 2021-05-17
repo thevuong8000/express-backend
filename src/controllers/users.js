5536c20b05bac5e2de3ec29294eb1db750223841
dante <ducmanh.tran2904@gmail.com>
2021-05-17 19:03:30 +0700

    feat: Encrypted password with bcrypt

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.getUsers = (req, res, next) => {
@@ -6,9 +7,10 @@ exports.getUsers = (req, res, next) => {
 		.catch((error) => res.status(400).json({ error }));
 };
 
-exports.createUser = (req, res, next) => {
+exports.createUser = async (req, res, next) => {
 	const { username, password, email } = req.body;
-	const user = new User({ username, password, email });
+	const hash = await bcrypt.hash(password, 10);
+	const user = new User({ username, password: hash, email });
 	user
 		.save()
 		.then((result) => res.status(201).json({ result, message: 'ok' }))
@@ -24,25 +26,33 @@ exports.getUsersById = (req, res, next) => {
 
 exports.updateUser = (req, res, next) => {
 	const { id } = req.params;
-	const { password, email } = req.body;
+	const { email } = req.body;
 
-	const newUser = new User({ _id: id, password, email });
+	const newUser = new User({ _id: id, email });
 	User.updateOne({ _id: id }, newUser)
 		.then(() => res.status(200).json({ message: 'Successfully modified' }))
-		.catch((error) => res.status(400).json({ error }));
+		.catch((error) => res.status(500).json({ error }));
 };
 
 exports.deleteUser = (req, res, next) => {
 	const { id } = req.params;
 
 	User.deleteOne({ _id: id })
-		.then((result) => res.status(200).json({ message: 'Successfully deleted' }))
+		.then(() => res.status(200).json({ message: 'Successfully deleted' }))
 		.catch((error) => res.status(400).json({ error }));
 };
 
-exports.loginUser = (req, res, next) => {
+exports.loginUser = async (req, res, next) => {
 	const { username, password } = req.body;
-	User.findOne({ username, password })
-		.then((result) => res.status(200).json({ result }))
-		.catch((error) => res.status(400).json({ error }));
+	try {
+		const user = await User.findOne({ username });
+		if (!user) return res.status(401).json({ error: 'User not found!' });
+
+		const validPassword = await bcrypt.compare(password, user.password);
+		return validPassword
+			? res.status(200).json({ userId: user._id, access_token: 'token' })
+			: res.status(401).json({ error: 'Incorrect password' });
+	} catch (error) {
+		return res.status(500).json({ error });
+	}
 };
