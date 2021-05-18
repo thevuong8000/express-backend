68c54ba8ecedabff0d15aea79a6bce0315a7280a
dante <ducmanh.tran2904@gmail.com>
2021-05-18 21:02:39 +0700

    fix: Generate token with default expiresIn is 2 hours

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { APP_CONFIG } = require('../constants/config');
 
-exports.generateToken = (payload, { expiresIn = '2h', ...restOptions }) =>
+exports.generateToken = (payload, { expiresIn = '2h', ...restOptions } = {}) =>
 	jwt.sign(payload, APP_CONFIG.JWT_KEY, { expiresIn, ...restOptions });
 
 exports.verifyToken = (token) => {
