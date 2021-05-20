ab87708fa504a50a037b45b4f2b4a768dde412d9
dante <ducmanh.tran2904@gmail.com>
2021-05-20 22:53:14 +0700

    refactor: Authentication endpoints

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -12,9 +12,7 @@ const {
 	getUsers,
 	getUsersById,
 	updateUser,
-	deleteUser,
-	loginUser,
-	refreshToken
+	deleteUser
 } = require('@controllers/users');
 
 /**
@@ -151,79 +149,5 @@ router.put('/:id', updateUser);
  *       description: Can not delete user
  */
 router.delete('/:id', deleteUser);
-
-/**
- * @swagger
- * /users/login:
- *  post:
- *   summary: User login.
- *   description: User login with username and password.
- *   tags:
- *     - User
- *   requestBody:
- *     required: true
- *     content:
- *       application/json:
- *         schema:
- *           type: object
- *           properties:
- *             name:
- *               type: string
- *             password:
- *               type: string
- *   responses:
- *     200:
- *       description: A list of users
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               _id:
- *                 type: string
- *               name:
- *                 type: string
- *               access_token:
- *                 type: string
- *               refresh_token:
- *                 type: string
- *     400:
- *       description: Can not create user
- */
-router.post('/login', loginUser);
-
-/**
- * @swagger
- * /users/refresh:
- *  post:
- *   summary: Refresh token.
- *   description: Refresh access token using refresh token.
- *   tags:
- *     - User
- *   requestBody:
- *     required: true
- *     content:
- *       application/json:
- *         schema:
- *           type: object
- *           properties:
- *             refresh_token:
- *               type: string
- *   responses:
- *     200:
- *       description: Get new access_token
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               _id:
- *                 type: string
- *               access_token:
- *                 type: string
- *     400:
- *       description: Refresh token is not valid
- */
-router.post('/refresh', refreshToken);
 
 module.exports = router;
