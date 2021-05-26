0abe33c974dfb41a988e3fa52f997c917bcfdc96
dante_cin <56090625+thevuong8000@users.noreply.github.com>
2021-05-26 21:38:04 +0700

    Migrate mongo cloud to mongoDB image (#11)
    * fix: Ignore data-db
    
    * fix: Using mongo image instead of mongo cloud
    
    * fix: Connect to mongo container

--- backend/mongodb/mongodb-config.js
+++ backend/mongodb/mongodb-config.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-const mongoUser = 'dante';
-const mongoPassword = 'fWd5BIGTlsmCwC2Q';
-const databaseName = 'userManagement';
-const MONGO_URI = `mongodb+srv://${mongoUser}:${mongoPassword}@test-mongo.e76gm.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+const MONGO_URI = 'mongodb://root:mongoadmin@mongodb:27017/express?authSource=admin&w=1';
 
 mongoose
 	.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 	.then(() => console.log('Connect mongoDB successfully!'))
-	.catch((error) => console.log('Fail to connect mongoDB!'));
+	.catch((error) => console.log('Fail to connect mongoDB!', error));
