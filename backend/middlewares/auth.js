6623a4bb11625dae7b5d777c4a3feb29b60bc05f
dante <ducmanh.tran2904@gmail.com>
2021-05-29 00:10:54 +0700

    feat: Handle pre-flight http method

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,22 @@
 const { verifyToken } = require('@utils/helper');
 
-const notAuthPaths = ['/', '/login'];
+const notAuthPaths = ['/', '/login', '/refresh-token', '/test-token'];
 
 module.exports = (req, res, next) => {
 	/* For easy dev, disable token verification for development mode */
-	if (process.env.NODE_ENV === 'development') return next();
+	// if (process.env.NODE_ENV === 'development') return next();
 
 	/* Skip authentication for login */
 	if (notAuthPaths.includes(req.path)) return next();
 
+	/* Resolve OPTIONS request */
+	if (req.method === 'OPTIONS') return res.status(200).send('Pre-flight resolved!');
+
 	try {
 		const token = req.headers.authorization.split(' ').pop();
 		const decodedToken = verifyToken(token);
+		return next();
 	} catch (error) {
-		next(error);
+		return next(error);
 	}
 };
