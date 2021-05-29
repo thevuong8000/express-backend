75cc9ae7e839fb5cb08d8b3176dea2618d7e7273
dante <ducmanh.tran2904@gmail.com>
2021-05-30 03:26:27 +0700

    refactor: Convert swagger docs to ts

---
+++ backend/swagger/swagger.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import { serve, setup } from 'swagger-ui-express';
+
+const router = Router();
+
+const swaggerUiOpts = {
+  explorer: true
+};
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Rest API',
+    version: '1.0.0',
+    contact: {
+      name: 'Manh Tran',
+      url: 'https://thevuong8000.github.io/',
+      email: 'ducmanh.tran2904@gmail.com'
+    }
+  },
+
+  /* Authentication */
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: 'apiKey',
+        description: 'JWT authorization of an API',
+        name: 'Authorization',
+        in: 'header'
+      }
+    }
+  },
+  security: [{ bearerAuth: [] as string[] }]
+};
+
+const swaggerDocOpts: swaggerJsdoc.Options = {
+  swaggerDefinition,
+  apis: ['./routes/api/*.js', './models/*.js']
+};
+
+const specs = swaggerJsdoc(swaggerDocOpts);
+// console.log(specs);
+router.use('/', serve, setup(specs, swaggerUiOpts));
+
+export default router;
