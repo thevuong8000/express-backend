3b5ee70468f70b17e6529e70f27fcd3d092e63a2
dante <ducmanh.tran2904@gmail.com>
2021-05-30 02:13:12 +0700

    fix: Type for node and express

--- backend/index.ts
+++ backend/index.ts
@@ -2,5 +2,5 @@ const app = require('./app');
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-	console.log(`Back-end listening at http://localhost:${port}`);
+	console.log(`Back-end a listening at http://localhost:${port}`);
 });
