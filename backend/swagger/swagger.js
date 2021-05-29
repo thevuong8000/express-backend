50c53579007d03ea82e161d36767cdfcf680788a
dante <ducmanh.tran2904@gmail.com>
2021-05-29 17:08:39 +0700

    feat: Authentication swagger UI

--- backend/swagger/swagger.js
+++ backend/swagger/swagger.js
@@ -18,7 +18,20 @@ const swaggerDefinition = {
 			url: 'https://thevuong8000.github.io/',
 			email: 'ducmanh.tran2904@gmail.com'
 		}
-	}
+	},
+
+	/* Authentication */
+	components: {
+		securitySchemes: {
+			bearerAuth: {
+				type: 'apiKey',
+				description: 'JWT authorization of an API',
+				name: 'Authorization',
+				in: 'header'
+			}
+		}
+	},
+	security: [{ bearerAuth: [] }]
 };
 
 const swaggerDocOpts = {
@@ -27,6 +40,7 @@ const swaggerDocOpts = {
 };
 
 const specs = swaggerJsdoc(swaggerDocOpts);
+// console.log(specs);
 router.use('/', swaggerUI.serve, swaggerUI.setup(specs, swaggerUiOpts));
 
 module.exports = router;
