a241778a69a2efd467bd5bd370669416615a8750
dante <ducmanh.tran2904@gmail.com>
2021-05-30 01:07:43 +0700

    fix: Comment for non-authorized request

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ const { verifyToken } = require('@utils/helper');
 const notAuthPaths = ['/', '/login', '/refresh-token', '/users/create'];
 
 module.exports = (req, res, next) => {
-	/* Skip authentication for login */
+	/* Skip authentication for non-authorized requests */
 	if (notAuthPaths.includes(req.path)) return next();
 
 	/* Resolve OPTIONS request */
