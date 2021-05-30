dc210316d8b2010296ef93023f036849d6850d84
dante <ducmanh.tran2904@gmail.com>
2021-05-30 14:00:41 +0700

    fix: Connect mongo container

--- backend/mongodb/mongodb-config.ts
+++ backend/mongodb/mongodb-config.ts
@@ -7,13 +7,8 @@ import {
 } from '../constants/config';
 import mongoose from 'mongoose';
 
-const mongoUser = 'dante';
-const mongoPassword = 'fWd5BIGTlsmCwC2Q';
-const databaseName = 'userManagement';
-const MONGO_URI = `mongodb+srv://${mongoUser}:${mongoPassword}@test-mongo.e76gm.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
-
-// const MONGO_URI = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}\
-// @${MONGODB_SERVICE}:${MONGODB_SERVICE_PORT}/${MONGO_INITDB_DATABASE}?authSource=admin`;
+const MONGO_URI = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}\
+@${MONGODB_SERVICE}:${MONGODB_SERVICE_PORT}/${MONGO_INITDB_DATABASE}?authSource=admin`;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
