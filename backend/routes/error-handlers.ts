01b1b443d219f506d42d9bcd2bb8b17c931227f3
dante <ducmanh.tran2904@gmail.com>
2021-06-04 14:27:08 +0700

    refactor: Error

--- backend/routes/error-handlers.ts
+++ backend/routes/error-handlers.ts
@@ -1,8 +1,11 @@
-import { IHttpRequestError } from 'schemas/error';
+import { IHttpRequestError } from '../schemas/error';
 import { NextFunction, Response, Request } from 'express';
 
-const errorHandler = (err: IHttpRequestError, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.code || 500).send({ message: err.message });
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof IHttpRequestError) {
+    return res.status(err.code).send({ message: err.message });
+  }
+  return next(err);
 };
 
 export default errorHandler;
