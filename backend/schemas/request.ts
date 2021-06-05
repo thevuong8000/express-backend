cea91889f9548863cd54610d880ff7e460ff40ab
dante <ducmanh.tran2904@gmail.com>
2021-06-05 11:23:34 +0700

    docs(auth-request): Add authenticated user information to Request

--- backend/schemas/request.ts
+++ backend/schemas/request.ts
@@ -1,6 +1,9 @@
 import { IUserDataToken } from './user';
 import { Request } from 'express';
 
+/**
+ * Authenticated User Information stored in Request
+ */
 export interface AuthRequest extends Request {
   auth: IUserDataToken;
 }
