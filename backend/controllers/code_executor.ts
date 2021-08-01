d0e6f55bed31460c748f2ef2c23be31f338bc98f
dante_cin <56090625+thevuong8000@users.noreply.github.com>
2021-08-01 22:48:43 +0700

    Code tester integrate input (#34)
    * fix: Allow unused local variables
    
    * chore(deps): Add uuid
    
    * feat: Add gitignore to tmp
    
    * feat: Receive input from UI and store to files
    
    * feat: Execute code and save output to files
    
    * fix: Ignore all files in tmp directory
    
    * feat: Add check result endpoint
    
    * fix: Send result containing all null if output not exist
    
    * feat: Logging for debugging
    
    * feat: Logging Error write output
    
    * refactor: Remove unused code

--- backend/controllers/code_executor.ts
+++ backend/controllers/code_executor.ts
@@ -1,40 +1,107 @@
 import { FILE_EXTENSIONS } from './../constants/code_executor';
 import { RequestHandler } from 'express';
 import { ICodeExecutorInput, Language } from '../routes/api/requests/code_executor';
-import { execSync } from 'child_process';
+import { exec, execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
-import { getCodeExecuteScript } from '../utils/code-executor';
+import { v4 as uuidv4 } from 'uuid';
+import { BadRequestError } from '../routes/api/responses/errors';
 
-interface ICodeExecutorOutPut {
-  code: number;
-  result: any;
-}
+/**
+ * Execute the submitted code.
+ * @param dir submission directory
+ * @param typedCode The code block to be executed
+ * @param language The language of the code
+ * @returns status code and result
+ */
+const executeCode = (
+  dir: string,
+  typedCode: string,
+  language: Language,
+  inputDir: string,
+  outputDir: string
+) => {
+  const fileName = path.resolve(dir, `test.${FILE_EXTENSIONS[language]}`);
+  fs.writeFileSync(fileName, typedCode);
 
-const helperExecuteCode = (
-  filename: string,
-  input: string,
-  language: Language
-): ICodeExecutorOutPut => {
-  try {
-    const script = getCodeExecuteScript(language)(filename);
-    const result = execSync(script, { encoding: 'utf-8' });
-    return { code: 200, result };
-  } catch (e) {
-    return { code: 400, result: e.stderr };
-  }
+  // Only for c++
+  const executableFile = path.resolve(dir, 'test');
+  execSync(`g++ -std=c++17 -o ${executableFile} ${fileName}`);
+  const files = fs.readdirSync(inputDir);
+  files.forEach((file) => {
+    exec(`${executableFile} < ${inputDir}/${file}`, (err, stdout, stderr) => {
+      const output = stderr ? stderr : stdout;
+      const outputFile = path.resolve(outputDir, file.toString());
+      fs.writeFile(outputFile, output, (err) => {
+        if (err) console.log('\tError write output', file, err);
+        else console.log('\tWrite output', file, 'successfully!');
+      });
+    });
+  });
 };
 
-export const compileAndExecuteCode: RequestHandler = (req, res, next) => {
-  const { typedCode, input, language } = <ICodeExecutorInput>req.body;
-  console.log('dummy', input, language);
-  console.log('dirname', __dirname);
-  const parentDir = path.resolve(__dirname, '../tmp');
-  const tempFile = path.resolve(parentDir, `test.${FILE_EXTENSIONS[language]}`);
+/**
+ * Create inputs directory with test file inside
+ * @param targetDir parent directory of input directory
+ * @param inputs
+ * @returns
+ */
+const setupInputs = (dir: string, inputs: string[]) => {
+  return Promise.all(
+    inputs.map((input, idx) => {
+      const filename = path.resolve(dir, idx.toString());
+      console.log('\tWrite input', idx, 'successfully!');
+      return fs.promises.writeFile(filename, input);
+    })
+  );
+};
 
-  fs.writeFileSync(tempFile, typedCode);
-  const data = helperExecuteCode(tempFile, input, language);
-  fs.unlink(tempFile, (err) => { if (err) console.log(err); });
+/**
+ * Process submitted code
+ */
+export const submitCode: RequestHandler = async (req, res, next) => {
+  const { typedCode, inputs, language } = <ICodeExecutorInput>req.body;
+  const submissionId = uuidv4();
 
-  res.status(data.code).json({ result: data.result });
+  // working with directory /backend/tmp/{submissionId}
+  const targetDir = path.resolve(__dirname, `../tmp/${submissionId}`);
+
+  if (fs.existsSync(targetDir))
+    next(new BadRequestError('Submission ID duplicated, please try again!'));
+
+  res.status(200).json({ submissionId });
+
+  fs.mkdirSync(targetDir);
+  console.log('Create submission directory:', targetDir);
+
+  const inputDir = path.resolve(targetDir, 'input');
+  fs.mkdirSync(inputDir);
+  console.log('Create input directory:', inputDir);
+  await setupInputs(inputDir, inputs);
+
+  const outputDir = path.resolve(targetDir, 'output');
+  fs.mkdirSync(outputDir);
+  console.log('Create output directory:', outputDir);
+  executeCode(targetDir, typedCode, language, inputDir, outputDir);
+};
+
+/**
+ * Check output of specific submission ID
+ */
+export const checkCodeResult: RequestHandler = (req, res, next) => {
+  const { submissionId, numTests } = req.body;
+  console.log('Check submission', submissionId);
+  const submissionDir = path.resolve(__dirname, `../tmp/${submissionId}`);
+  const outputDir = path.resolve(submissionDir, 'output');
+
+  const result = Array(numTests);
+  if (!fs.existsSync(outputDir)) return res.status(200).json({ result });
+
+  const files = fs.readdirSync(outputDir);
+  files.forEach((file) => {
+    const filePath = path.resolve(outputDir, file);
+    const idx = parseInt(file);
+    result[idx] = fs.readFileSync(filePath, { encoding: 'utf-8' });
+  });
+  res.status(200).json({ result });
 };
--- backend/routes/api/requests/code_executor.ts
+++ backend/routes/api/requests/code_executor.ts
@@ -3,6 +3,6 @@ export type Language = 'javascript' | 'typescript' | 'cpp' | 'python' | 'java';
 export interface ICodeExecutorInput {
   typedCode: string;
   language: Language;
-  input: string;
+  inputs: string[];
 }
 
