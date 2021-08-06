eac91571a0cc4b78aab59102ab58923a78ac6c6d
dante <ducmanh.tran2904@gmail.com>
2021-08-06 10:41:20 +0700

    fix: Remove /login and /users endpoint in standalone

--- backend/routes/api/index.ts
+++ backend/routes/api/index.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import loginRoutes from './login';
 import userRoutes from './users';
 import codeExecutorRoutes from './code_executor';
+import { IS_STANDALONE } from '../../constants/config';
 
 const router = Router();
 
-router.use('/login', loginRoutes);
-router.use('/users', userRoutes);
+if (!IS_STANDALONE) {
+  router.use('/login', loginRoutes);
+  router.use('/users', userRoutes);
+}
 router.use('/code-executor', codeExecutorRoutes);
 
 export default router;
