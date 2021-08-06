9afb050d2b926ad372bc88374de541b3e372a08e
dante <ducmanh.tran2904@gmail.com>
2021-08-06 10:36:08 +0700

    fix: Do not autheticate if standalone

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import auth from './auth';
 import apiRoutes from 'api';
 import errorHandlers from './error-handlers';
+import { IS_STANDALONE } from '../constants/config';
 
 const router = Router();
 
 // Authentication
-router.use(auth);
+// Only authenticate if not standalone
+if (!IS_STANDALONE) router.use(auth);
 
 // API routes
 router.use('/', apiRoutes);
