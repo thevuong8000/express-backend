1bfb63d3fe45efd8731ef8a29701d0273b879b6f
dante <ducmanh.tran2904@gmail.com>
2021-08-15 16:27:55 +0700

    refactor: Using class for executor

---
+++ backend/utils/code-executor/codeExecutor.ts
@@ -0,0 +1,258 @@
+import { ISubmission } from '../../routes/api/requests/code_executor';
+import { SubmissionFileManager } from './submisisonFileManager';
+import fs from 'fs';
+import { isCompiledLanguage, TEMP_SUBMISSION_PARENT_DIRECTORY } from '../code-executor';
+import { BadRequestError } from '../../routes/api/responses/errors';
+import path from 'path';
+import { exec, execSync } from 'child_process';
+import waitUntil from 'async-wait-until';
+
+/**
+ * Create {tmp} submisison directory if not exist
+ */
+const createTempSubmissionParentDirectoryIfNotExist = () => {
+  if (!fs.existsSync(TEMP_SUBMISSION_PARENT_DIRECTORY)) {
+    console.log('Creating tmp directory');
+    fs.mkdirSync(TEMP_SUBMISSION_PARENT_DIRECTORY);
+    console.log('Successfully created tmp directory');
+  }
+};
+
+interface ICodeExecutorConstructor extends ISubmission {
+  submissionId: string;
+}
+
+export class CodeExecutor extends SubmissionFileManager {
+  /**
+   * Setup submission input directory
+   */
+  private setupSubmissionInputDirectory: () => void;
+
+  /**
+   * Setup submission output directory
+   */
+  private setupSubmissionOutputDirectory: () => void;
+
+  /**
+   * Write user's inputs into separated files
+   */
+  private writeInputsIntoFiles: () => void;
+
+  /**
+   * Get the output for user's code in Regular Mode
+   */
+  private getRegularModeOutput: () => string;
+
+  /**
+   * Execute user's code in Competitive Programming Mode
+   */
+  private executeCodeCompetitiveProgrammingMode: () => void;
+
+  /**
+   * Execute user's code in Regular Mode
+   */
+  private executeCodeRegularMode: () => void;
+
+  /**
+   * Handle compile error
+   */
+  private handleCompileError: (error: any) => void;
+
+  /**
+   * Compile file if the language is compiled language
+   * Do nothing if the language is not compiled language
+   */
+  private compileFileIfNeeded: () => void;
+
+  /**
+   * Get general execute script
+   * For example:
+   * 1) python      => python3 user.py
+   * 2) javascript  => node user.js
+   * 3) cpp         => g++ --std=c++14 -o objectFile user.cpp
+   */
+  private getExecuteScript: () => string;
+
+  /**
+   * Write user's code into file synchronous
+   * Write file sync is necessary since code needs to be written before being executed
+   */
+  private writeUserCodeIntoFile: () => void;
+
+  /**
+   * Check if executor is ready to execute user's code
+   */
+  private isExecutorReady: () => boolean;
+
+  /**
+   * Set up submission
+   * 1) Set up directory for submission, input, output
+   * 2) Write data into files
+   */
+  public setupSubmission: () => Promise<void>;
+
+  /**
+   * Execute the user's code
+   */
+  public execute: () => void;
+
+  constructor({ submissionId, typedCode, language, inputs, mode }: ICodeExecutorConstructor) {
+    super({ submissionId, language });
+
+    this.setupSubmissionInputDirectory = () => {
+      const inputDir = this.getSubmissionInputDirectory();
+      console.log('Creating input directory...');
+      fs.mkdir(inputDir, (err) => {
+        if (err) console.log('Create submission directory error:', err);
+        this.writeInputsIntoFiles();
+      });
+      console.log('Create input directory successfully:', inputDir);
+    };
+
+    this.setupSubmissionOutputDirectory = () => {
+      const outputDir = this.getSubmissionOutputDirectory();
+      console.log('Creating output directory...');
+      fs.mkdirSync(outputDir);
+      console.log('Create output directory successfully:', outputDir);
+    };
+
+    this.setupSubmission = async () => {
+      createTempSubmissionParentDirectoryIfNotExist();
+
+      const submissionDir = this.getSubmissionDirectory();
+
+      console.log('Creating submission directory:', submissionDir);
+      if (fs.existsSync(submissionDir)) {
+        throw new BadRequestError('Submission ID duplicated, please try again!');
+      }
+      fs.mkdir(submissionDir, (err) => {
+        if (err) console.log('Create submission directory error:', err);
+        this.writeUserCodeIntoFile();
+        this.setupSubmissionInputDirectory();
+        this.setupSubmissionOutputDirectory();
+      });
+    };
+
+    this.writeUserCodeIntoFile = () => {
+      const fileName = this.getCodeFileName();
+      fs.writeFile(fileName, typedCode, (err) => {
+        if (err) console.log("Cannot write user's code into file");
+      });
+    };
+
+    this.writeInputsIntoFiles = () => {
+      const inputDir = this.getSubmissionInputDirectory();
+      inputs.map((testCase) => {
+        const filename = path.resolve(inputDir, testCase.id);
+        fs.writeFile(filename, testCase.input, (err) => {
+          if (err) console.log(`\tCan not write ${testCase.id}`);
+          else console.log(`\tWrite input ${testCase.id} successfully into ${filename}`);
+        });
+      });
+    };
+
+    this.getRegularModeOutput = () => {
+      const outputFile = this.getRegularModeOutputFileName();
+      return fs.readFileSync(outputFile, { encoding: 'utf-8' });
+    };
+
+    this.getExecuteScript = () => {
+      const filePath = this.getPathToExecutableFile();
+      switch (language) {
+        case 'cpp':
+          return filePath;
+
+        case 'java':
+          return `java ${filePath}`;
+
+        case 'python':
+          return `python3 ${filePath}`;
+
+        case 'javascript':
+          return `node ${filePath}`;
+
+        default:
+          return 'ThisIsNotAValidScript';
+      }
+    };
+
+    this.executeCodeRegularMode = () => {
+      console.log('Executing code in Regular Mode');
+      const outputFile = this.getRegularModeOutputFileName();
+      const execScript = this.getExecuteScript();
+
+      exec(execScript, (err, stdout, stderr) => {
+        if (stderr) {
+          console.log('error', stderr);
+          return;
+        }
+
+        fs.writeFile(outputFile, stdout, (err) => {
+          if (err) console.log('Regular Submision Mode: Can not write file');
+          else console.log('Output for Regular Mode is ready');
+        });
+      });
+    };
+
+    this.executeCodeCompetitiveProgrammingMode = () => {
+      console.log('Executing code in Competitive Programming Mode');
+      const outputDir = this.getSubmissionOutputDirectory();
+      const inputDir = this.getSubmissionInputDirectory();
+      const inputFiles = fs.readdirSync(inputDir);
+      const execScript = this.getExecuteScript();
+
+      inputFiles.forEach((file) => {
+        const inputFilePath = path.resolve(inputDir, file);
+        exec(`${execScript} < ${inputFilePath}`, (err, stdout, stderr) => {
+          const output = stderr ? stderr : stdout;
+
+          // corresponding output would have the same name
+          const outputFilePath = path.resolve(outputDir, file);
+          fs.writeFile(outputFilePath, output, (err) => {
+            if (err) console.log('\tError write output', file, err);
+            else console.log('\tWrite output', file, 'successfully!');
+          });
+        });
+      });
+    };
+
+    this.isExecutorReady = () => {
+      const userFilePath = this.getCodeFileName();
+      return fs.existsSync(userFilePath);
+    };
+
+    // TODO: currently only support c++
+    this.compileFileIfNeeded = () => {
+      if (!isCompiledLanguage(language)) return;
+
+      const userFilePath = this.getCodeFileName();
+      const objectFileName = this.getCompiledCodeFileName();
+      execSync(`g++ -std=c++17 -o ${objectFileName} ${userFilePath}`);
+    };
+
+    this.handleCompileError = (error) => {
+      const compileErrorPath = this.getPathToCompileErrorFile();
+      fs.writeFileSync(compileErrorPath, error.stderr, { encoding: 'utf-8' });
+    };
+
+    this.execute = async () => {
+      await waitUntil(this.isExecutorReady, { timeout: 5000 });
+      try {
+        this.compileFileIfNeeded();
+      } catch (err) {
+        this.handleCompileError(err);
+      }
+
+      switch (mode) {
+        case 'Regular':
+          return this.executeCodeRegularMode();
+
+        case 'Compatitive Programming':
+          return this.executeCodeCompetitiveProgrammingMode();
+
+        default:
+          break;
+      }
+    };
+  }
+}
---
+++ backend/utils/code-executor/submisisonFileManager.ts
@@ -0,0 +1,111 @@
+import { Language } from '../../routes/api/requests/code_executor';
+import { isCompiledLanguage, TEMP_SUBMISSION_PARENT_DIRECTORY } from '../code-executor';
+import { FILE_EXTENSIONS } from '../../constants/code_executor';
+import path from 'path';
+
+interface ISubmissionFileManagerConstructor {
+  submissionId: string;
+  language: Language;
+}
+
+export class SubmissionFileManager {
+  static inputDirName = 'input';
+  static outputDirName = 'output';
+  static userFileName = 'user';
+  static userObjectFileName = 'user-object';
+  static compileErrorFileName = 'compile-error';
+
+  /**
+   * Get the path to target submission directory
+   * @returns the path to target submission directory
+   */
+  protected getSubmissionDirectory: () => string;
+
+  /**
+   * Get the input directory of target submission
+   * @returns the path to target input directory
+   */
+  protected getSubmissionInputDirectory: () => string;
+
+  /**
+   * Get the output directory of target submission
+   * @returns the path to target output directory
+   */
+  protected getSubmissionOutputDirectory: () => string;
+
+  /**
+   * Get the user's code file name
+   */
+  protected getCodeFileName: () => string;
+
+  /**
+   * Get the path to object file (only if code in compiled language)
+   */
+  protected getCompiledCodeFileName: () => string;
+
+  /**
+   * Get final executable file
+   */
+  protected getRegularModeOutputFileName: () => string;
+
+  /**
+   * Get the path to output result of submission in Regular Mode
+   * @returns the path to output file Regular Mode
+   */
+  protected getPathToExecutableFile: () => string;
+
+  /**
+   * Get the full user file name with file extension
+   */
+  protected getFullUserFileName: () => string;
+
+  /**
+   * Get the path to compile error file
+   */
+  protected getPathToCompileErrorFile: () => string;
+
+  constructor({ submissionId, language }: ISubmissionFileManagerConstructor) {
+    this.getSubmissionDirectory = () => {
+      return path.resolve(TEMP_SUBMISSION_PARENT_DIRECTORY, submissionId);
+    };
+
+    this.getSubmissionInputDirectory = () => {
+      const submissionDir = this.getSubmissionDirectory();
+      return path.resolve(submissionDir, SubmissionFileManager.inputDirName);
+    };
+
+    this.getSubmissionOutputDirectory = () => {
+      const submissionDir = this.getSubmissionDirectory();
+      return path.resolve(submissionDir, SubmissionFileManager.outputDirName);
+    };
+
+    this.getCodeFileName = () => {
+      const submissionDir = this.getSubmissionDirectory();
+      const fileName = this.getFullUserFileName();
+      return path.resolve(submissionDir, fileName);
+    };
+
+    this.getCompiledCodeFileName = () => {
+      const submissionDir = this.getSubmissionDirectory();
+      return path.resolve(submissionDir, SubmissionFileManager.userObjectFileName);
+    };
+
+    this.getFullUserFileName = () => {
+      return `${SubmissionFileManager.userFileName}.${FILE_EXTENSIONS[language]}`;
+    };
+
+    this.getPathToExecutableFile = () => {
+      return isCompiledLanguage(language) ? this.getCompiledCodeFileName() : this.getCodeFileName();
+    };
+
+    this.getRegularModeOutputFileName = () => {
+      const outputDir = this.getSubmissionOutputDirectory();
+      return path.resolve(outputDir, './output');
+    };
+
+    this.getPathToCompileErrorFile = () => {
+      const outputDir = this.getSubmissionOutputDirectory();
+      return path.resolve(outputDir, SubmissionFileManager.compileErrorFileName);
+    };
+  }
+}
